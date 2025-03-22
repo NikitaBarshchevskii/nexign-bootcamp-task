6f511f6c7dd65a9f5fb9a6309ca475b62f941fc9
Nikita Barshchevskii <nukuta092002@mail.ru>
2025-03-23 01:49:17 +0300

    Initial commit

---
+++ src/main/java/ru/nexign/bootcamptask/NexignBootcampTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.nexign.bootcamptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexignBootcampTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexignBootcampTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nexign/bootcamptask/NexignBootcampTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nexign.bootcamptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexignBootcampTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
